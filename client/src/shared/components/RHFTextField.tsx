129c99e4c4315f8d6d8e9e21f9783ba690f54651
Arjohn15 <arjohn15@gitAccount>
2025-05-24 18:44:11 +0800

    responsive register and login updated

--- client/src/shared/components/RHFTextField.tsx
+++ client/src/shared/components/RHFTextField.tsx
@@ -1,4 +1,4 @@
-import { TextField, useMediaQuery } from "@mui/material";
+import { TextField } from "@mui/material";
 import { Controller, useFormContext } from "react-hook-form";
 
 interface TextFieldProps {
